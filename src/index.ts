59f3e7a479283773d6371a4d5d672472a3f2656d
horihiro <hori.kawa.shima.tool+github@gmail.com>
2022-12-23 21:09:40 +0000

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { app, HttpRequest, InvocationContext } from "@azure/functions";
+import { app, HttpRequest } from "@azure/functions";
 import puppeteer from "puppeteer";
 
 const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
 
 app.http('url2pdf', {
   methods: ['GET'],
-  handler: async (_: InvocationContext, request: HttpRequest) => {
+  handler: async (request: HttpRequest) => {
     const page = await browser.newPage();
     await page.goto(request.query.get('url') || 'https://www.microsoft.com/en-us/', { waitUntil: 'networkidle2' });
     const pdf = await page.pdf();
